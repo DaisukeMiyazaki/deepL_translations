ded681d738a8bd38735d1eaa528ede345ebf50f1
Daisuke <aubozumom@icloud.com>
2022-09-17 10:53:59 +0900

    1 - removing function nesting

--- src/main.js
+++ src/main.js
@@ -2,36 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 // const { Octokit } = require('@octokit/rest');
 
-async function run() {
+try {
   const ms = core.getInput('input_testing');
-  try {
-    core.debug('Inside try block');
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
+  core.debug('Inside try block');
+  const time = new Date().toTimeString();
+  core.setOutput('time', time);
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 
-    if (!ms) {
-      core.warning('testInput was not set');
-    }
-
-    if (core.isDebug()) {
-      // curl -v https://github.com
-      core.warning('isDebug is true');
-    } else {
-      // curl https://github.com
-      core.warning('isDebug is false');
-    }
-
-    // Do stuff
-    core.info('Output to the actions build log');
-
-    core.notice('This is a message that will also emit an annotation');
-  } catch (error) {
-    core.setFailed(error.message);
+  if (!ms) {
+    core.warning('testInput was not set');
   }
-}
 
-run();
+  if (core.isDebug()) {
+    // curl -v https://github.com
+    core.warning('isDebug is true');
+  } else {
+    // curl https://github.com
+    core.warning('isDebug is false');
+  }
+
+  // Do stuff
+  core.info('Output to the actions build log');
+
+  core.notice('This is a message that will also emit an annotation');
+} catch (error) {
+  core.setFailed(error.message);
+}

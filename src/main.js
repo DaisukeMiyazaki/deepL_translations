085425d17c53c884bc56444164e22e4135f3492c
Daisuke <aubozumom@icloud.com>
2022-09-17 14:17:56 +0900

    4-use deep-node instead

--- src/main.js
+++ src/main.js
@@ -1,33 +1,37 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 // const { Octokit } = require('@octokit/rest');
+const deepl = require('deepl-node');
 
 try {
   const ms = core.getInput('input_testing');
   const API_KEY = core.getInput('DEEPL_TOKEN');
-  const API_URL = 'https://api-free.deepl.com/v2/translate';
+  const translator = new deepl.Translator(API_KEY);
 
-  let content = encodeURI('auth_key=' + API_KEY + '&text=' + ms + '&source_lang=JA&target_lang=EN');
-  let url = API_URL + '?' + content;
+  // let content = encodeURI('auth_key=' + API_KEY + '&text=' + ms + '&source_lang=JA&target_lang=EN');
+  // let url = API_URL + '?' + content;
 
-  fetch(url)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Could not reach the API: ' + response.statusText);
-      }
-    })
-    .then(function (data) {
-      translated_result = data['translations'][0]['text'];
-      core.setOutput('output_testing_success', translated_result);
-    })
-    .catch(function (error) {
-      error_message = error.message;
-      core.setOutput('output_testing_error', error_message);
-    });
+  // fetch(url)
+  //   .then(function (response) {
+  //     if (response.ok) {
+  //       return response.json();
+  //     } else {
+  //       throw new Error('Could not reach the API: ' + response.statusText);
+  //     }
+  //   })
+  //   .then(function (data) {
+  //     translated_result = data['translations'][0]['text'];
+  //     core.setOutput('output_testing_success', translated_result);
+  //   })
+  //   .catch(function (error) {
+  //     error_message = error.message;
+  //     core.setOutput('output_testing_error', error_message);
+  //   });
+
+  const result = translator.translateText(ms, 'JA', 'EN');
 
   console.log('Input: ', ms);
+  core.setOutput('output_testing_success', result);
   core.debug('Inside try block');
   const time = new Date().toTimeString();
   core.setOutput('time', time);

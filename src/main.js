42e292df14bb9e258a28022ce656c31d32561a67
Daisuke <aubozumom@icloud.com>
2022-09-17 09:55:59 +0900

    1 - using core debug instead of console

--- src/main.js
+++ src/main.js
@@ -3,9 +3,26 @@ const core = require('@actions/core');
 // const { Octokit } = require('@octokit/rest');
 
 async function run() {
+  const ms = core.getInput('testInput');
   try {
-    const ms = core.getInput('testInput');
-    console.log(ms);
+    core.debug('Inside try block');
+
+    if (!ms) {
+      core.warning('testInput was not set');
+    }
+
+    if (core.isDebug()) {
+      // curl -v https://github.com
+      core.warning('isDebug is true');
+    } else {
+      // curl https://github.com
+      core.warning('isDebug is false');
+    }
+
+    // Do stuff
+    core.info('Output to the actions build log');
+
+    core.notice('This is a message that will also emit an annotation');
   } catch (error) {
     core.setFailed(error.message);
   }

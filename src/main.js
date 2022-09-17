081f1a8ceb2946722d44d8669793b2934887993f
Daisuke <aubozumom@icloud.com>
2022-09-17 09:38:05 +0900

    1 - check testInput works

--- src/main.js
+++ src/main.js
@@ -4,16 +4,8 @@ const { Octokit } = require('@octokit/rest');
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString());
-    await new Promise(resolve => {
-      setTimeout(() => resolve('done!'), 10);
-    });
-    core.debug(new Date().toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const ms = core.getInput('testInput');
+    console.log(ms)
   } catch (error) {
     core.setFailed(error.message);
   }

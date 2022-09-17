916f7f6235d29c9139d1c210d6d5e283c5140c0b
Daisuke <aubozumom@icloud.com>
2022-09-17 10:42:46 +0900

    1- redefining testing inputs

--- dist/index.js
+++ dist/index.js
@@ -2807,9 +2807,26 @@ const core = __nccwpck_require__(186);
 // const { Octokit } = require('@octokit/rest');
 
 async function run() {
+  const ms = core.getInput('testInput');
   try {
-    const ms = core.getInput('testInput');
-    console.log(ms);
+    core.debug('Inside try block');
+
+    if (!ms) {
+      core.warning('testInput was not set');
+    }
+
+    if (core.isDebug()) {
+      // curl -v https://github.com
+      core.warning('isDebug is true');
+    } else {
+      // curl https://github.com
+      core.warning('isDebug is false');
+    }
+
+    // Do stuff
+    core.info('Output to the actions build log');
+
+    core.notice('This is a message that will also emit an annotation');
   } catch (error) {
     core.setFailed(error.message);
   }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
+const github = require('@actions/github');
 // const { Octokit } = require('@octokit/rest');
 
 async function run() {
-  const ms = core.getInput('testInput');
+  const ms = core.getInput('input_testing');
   try {
     core.debug('Inside try block');
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 
     if (!ms) {
       core.warning('testInput was not set');

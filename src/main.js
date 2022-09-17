a463fae460ba96f5e405c2028682542b609e8c59
Daisuke <aubozumom@icloud.com>
2022-09-17 09:48:46 +0900

    1 - fix pritter

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const { Octokit } = require('@octokit/rest');
+// const github = require('@actions/github');
+// const { Octokit } = require('@octokit/rest');
 
 async function run() {
   try {
     const ms = core.getInput('testInput');
-    console.log(ms)
+    console.log(ms);
   } catch (error) {
     core.setFailed(error.message);
   }
